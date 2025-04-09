2a618512603de8ef461b236e02ad025f7404aa7c
drewandrewd <andrewid10@gmail.com>
2025-04-09 12:11:26 +0500

    Added cache

--- weather/src/main/java/com/example/weather/controller/WeatherController.java
+++ weather/src/main/java/com/example/weather/controller/WeatherController.java
@@ -1,40 +1,30 @@
 package com.example.weather.controller;
 
-import com.example.weather.model.Weather;
-import com.example.weather.repository.WeatherRepository;
+import com.example.weather.model.Main;
+import com.example.weather.model.Root;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/weather")
 public class WeatherController {
 
     @Autowired
-    private WeatherRepository weatherRepository;
+    private RestTemplate restTemplate;
+    @Value("${appid}")
+    private String appId;
+    @Value("${url.weather}")
+    private String urlWeather;
 
-    @GetMapping
-    public Iterable<Weather> findAll() {
-        return weatherRepository.findAll();
-    }
-
-    @GetMapping("/{id}")
-    public Optional<Weather> findById(@PathVariable int id) {
-        return weatherRepository.findById(id);
-    }
-
-    @PostMapping
-    public ResponseEntity<Weather> save(@RequestBody Weather weather) {
-        return weatherRepository.findById(weather.getId()).isPresent()
-                ? new ResponseEntity<>(HttpStatus.BAD_REQUEST)
-                : new ResponseEntity<>(weatherRepository.save(weather), HttpStatus.CREATED);
+    @Cacheable(value = "weather", key = "#lat + '-' + #lon")
+    @GetMapping("/weather")
+    public Main getWeather(@RequestParam String lat, @RequestParam String lon) {
+        String request = String.format("%s?lat=%s&lon=%s&units=metric&appid=%s",
+                urlWeather, lat, lon, appId);
+        Root response = restTemplate.getForObject(request, Root.class);
+        System.out.println("fghhgsd");
+        return response != null ? response.getMain() : null;
     }
 }
--- weather/src/main/java/com/example/weather/model/Weather.java
+++ weather/src/main/java/com/example/weather/model/Weather.java
@@ -3,72 +3,25 @@ package com.example.weather.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import lombok.*;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class Weather {
 
     @Id
     @GeneratedValue
     private int id;
 
-    @NonNull
-    private double latitude;
-    @NonNull
-    private double longitude;
-    @NonNull
-    private String condition;
-    @NonNull
-    private double temperature;
+    private String main;
+    private String description;
+    private String icon;
 
-    public Weather(@NonNull double latitude, @NonNull double longitude, @NonNull String condition, @NonNull double temperature) {
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.condition = condition;
-        this.temperature = temperature;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-    public String getCondition() {
-        return condition;
-    }
-
-    public void setCondition(String condition) {
-        this.condition = condition;
-    }
-
-    public double getTemperature() {
-        return temperature;
-    }
-
-    public void setTemperature(double temperature) {
-        this.temperature = temperature;
+    public Weather(String main, String description, String icon) {
+        this.main = main;
+        this.description = description;
+        this.icon = icon;
     }
 }
