cb04ae115275bc4e959c0d9967c6db9b36b3222e
drewandrewd <andrewid10@gmail.com>
2025-04-11 00:20:27 +0500

    added new methods to controller

--- location/src/main/java/com/example/location/repository/LocationRepository.java
+++ location/src/main/java/com/example/location/repository/LocationRepository.java
@@ -4,6 +4,10 @@ import com.example.location.model.Location;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface LocationRepository extends CrudRepository<Location, Integer> {
+public interface LocationRepository extends CrudRepository<Location, Long> {
+    Optional<Location> findByName(String name);
+    boolean existsByName(String name);
 }
